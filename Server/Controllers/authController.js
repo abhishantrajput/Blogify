2db852da299d340c48ad5ca573701d9cf77062d3
Abhishant Rajput <abhishantsinghrajput@gmai.com>
2024-04-10 11:34:17 +0530

    Created Error handler Middlewares

--- Server/Controllers/authController.js
+++ Server/Controllers/authController.js
@@ -1,34 +1,33 @@
 import User from "../Models/userSchema.js";
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const Register = async (req, res) => {
+export const Register = async (req, res,next) => {
   try {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username == "" || email == "" || password == "") {
-      return res.status(400).json({
-        message: "All fields are Required",
-      });
+     return next(errorHandler(400,"All fields are Required"))
     }
 
-    const user1 = await User.find({ email });
+    // const user1 = await User.find({ email });
 
-    const exitstedUsername = await User.find({ username });
+    // const exitstedUsername = await User.find({ username });
 
-    if (exitstedUsername.length > 0) {
-      return res.status(400).json({
-        message: "username  not Available",
-      });
-    }
+    // if (exitstedUsername?.length > 0) {
+    //   return res.status(400).json({
+    //     message: "username  not Available",
+    //   });
+    // }
 
-    if (user1.length > 0) {
-      return res.status(400).json({
-        message: "Email already Exists",
-      });
-    }
+    // if (user1?.length > 0) {
+    //   return res.status(400).json({
+    //     message: "Email already Exists",
+    //   });
+    // }
 
 
-    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
       username,
@@ -43,9 +42,7 @@ export const Register = async (req, res) => {
       data: user,
     });
   } catch (error) {
-    return res.status(400).json({
-      message: "Somthing Error got Occured",
-      Error: error,
-    });
+   
+    next(error)
   }
 };
--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { Database_Connection } from "./Database/Connection.js";
 import userRoute from "./Routes/userRoute.js";
-import authRoute from "./Routes/authRoute.js"
+import authRoute from "./Routes/authRoute.js";
 
 const app = express();
 
@@ -10,17 +10,27 @@ const PORT = 8000;
 
 dotenv.config();
 
-
 // important middlewares
 
-
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 
 app.use("/api/users", userRoute);
-app.use("/api/auth",authRoute)
+app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 Database_Connection();
 
